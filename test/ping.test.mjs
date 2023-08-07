b00709a9c0b85b69782be07d6fe8a72b77aa7b5d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-08-07 09:54:50 +0800

    test: fix resolved module path on Windows

--- test/ping.test.mjs
+++ test/ping.test.mjs
@@ -1,11 +1,12 @@
 import esmock from "esmock";
 import { createRequire } from "module";
+import path from "path";
 
 const require = createRequire(import.meta.url);
 
 it("should not ping the localhost", async () => {
   const { pingLocalhost } = await esmock("../dist/ping.mjs", {
-    [require.resolve("ping")]: {
+    [require.resolve("ping").replaceAll(path.sep, "/")]: {
       promise: {
         probe: async () => ({ alive: true }),
       },
