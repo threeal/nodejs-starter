34763e3191a3765e26118014ca6a15417e35fc22
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-20 11:04:34 +0700

    test: move test files to the `src` directory

--- src/ping.test.ts
+++ src/ping.test.ts
@@ -11,13 +11,13 @@ jest.unstable_mockModule("ping", () => ({
 
 describe("localhost ping", () => {
   it("should ping the localhost", async () => {
-    const { pingLocalhost } = await import("../src/ping.mjs");
+    const { pingLocalhost } = await import("./ping.mjs");
     stubs.ping.promise.probe.resolves({ alive: true });
     return expect(pingLocalhost()).resolves.toBe(true);
   });
 
   it("should not ping the localhost", async () => {
-    const { pingLocalhost } = await import("../src/ping.mjs");
+    const { pingLocalhost } = await import("./ping.mjs");
     stubs.ping.promise.probe.resolves({ alive: false });
     return expect(pingLocalhost()).resolves.toBe(false);
   });
