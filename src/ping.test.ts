39468e215cc3b43073e7e38da6a530e51525efd2
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-20 11:04:36 +0700

    test: replace `sinon.stub()` with `jest.fn()`

--- src/ping.test.ts
+++ src/ping.test.ts
@@ -1,24 +1,21 @@
 import { jest } from "@jest/globals";
-import sinon from "sinon";
 
-const stubs = {
-  ping: { promise: { probe: sinon.stub() } },
-};
+const mockedPing = { promise: { probe: jest.fn() } };
 
 jest.unstable_mockModule("ping", () => ({
-  default: stubs.ping,
+  default: mockedPing,
 }));
 
 describe("localhost ping", () => {
   it("should ping the localhost", async () => {
     const { pingLocalhost } = await import("./ping.mjs");
-    stubs.ping.promise.probe.resolves({ alive: true });
+    mockedPing.promise.probe.mockReturnValue(Promise.resolve({ alive: true }));
     return expect(pingLocalhost()).resolves.toBe(true);
   });
 
   it("should not ping the localhost", async () => {
     const { pingLocalhost } = await import("./ping.mjs");
-    stubs.ping.promise.probe.resolves({ alive: false });
+    mockedPing.promise.probe.mockReturnValue(Promise.resolve({ alive: false }));
     return expect(pingLocalhost()).resolves.toBe(false);
   });
 });
