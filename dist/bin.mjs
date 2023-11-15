b00e685bc75a7c5dbd79042ad79bdb04b57104e9
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-15 14:23:29 +0700

    feat: add `bin.mts` executable file

---
+++ dist/bin.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { fibonacciSequence } from "./sequence.mjs";
+const n = Number.parseInt(process.argv[2]);
+const sequence = fibonacciSequence(n);
+process.stdout.write(sequence.join(" "));
+//# sourceMappingURL=bin.mjs.map
