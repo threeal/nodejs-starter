ad2a2549c0ea02963c8a4a864b6ccf80602f247c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-23 20:10:12 +0700

    feat: replace Yargs with Commander
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/bin.ts
+++ src/bin.ts
@@ -1,23 +1,15 @@
 #!/usr/bin/env node
 
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import { program } from "commander";
 import { fibonacciSequence } from "./sequence.js";
 
-yargs(hideBin(process.argv))
-  .scriptName("my_fibonacci")
+program
+  .name("my_fibonacci")
   .version("0.0.0")
-  .command(
-    "$0 <n>",
-    "Generate a Fibonacci sequence up to the given number of terms.",
-    (yargs) =>
-      yargs.positional("n", {
-        demandOption: true,
-        describe: "The number of terms",
-        type: "number",
-      }),
-    (argv) => {
-      process.stdout.write(fibonacciSequence(argv.n).join(" ") + "\n");
-    },
-  )
+  .description("Generate a Fibonacci sequence up to the given number of terms.")
+  .argument("<n>", "The number of terms", parseInt)
+  .action((n) => {
+    const sequence = fibonacciSequence(n);
+    process.stdout.write(`${sequence.join(" ")}\n`);
+  })
   .parse();
